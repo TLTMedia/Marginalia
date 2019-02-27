7b52e2c0dfd2cac3b099267c50c647643cdd8517
shortland <ilankleiman@gmail.com>
2019-02-27 18:03:59 -0500

    feat: installed slim api

---
+++ api/public/index.php
@@ -0,0 +1,51 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+date_default_timezone_set('America/New_York');
+
+require '../vendor/autoload.php';
+
+// Prepare app
+$app = new \Slim\Slim(array(
+    'templates.path' => '../templates',
+));
+
+// Create monolog logger and store logger in container as singleton
+// (Singleton resources retrieve the same log resource definition each time)
+$app->container->singleton('log', function () {
+    $log = new \Monolog\Logger('slim-skeleton');
+    $log->pushHandler(new \Monolog\Handler\StreamHandler('../logs/app.log', \Monolog\Logger::DEBUG));
+    return $log;
+});
+
+// Prepare view
+$app->view(new \Slim\Views\Twig());
+$app->view->parserOptions = array(
+    'charset' => 'utf-8',
+    'cache' => realpath('../templates/cache'),
+    'auto_reload' => true,
+    'strict_variables' => false,
+    'autoescape' => true
+);
+$app->view->parserExtensions = array(new \Slim\Views\TwigExtension());
+
+// Define routes
+$app->get('/', function () use ($app) {
+    // Sample log message
+    $app->log->info("Slim-Skeleton '/' route");
+    // Render index view
+    $app->render('index.html');
+});
+
+$app->get('/helloworld', function () use ($app) {
+    // Sample log message
+    $app->log->info("Slim-Skeleton '/' route");
+    // Render index view
+    //$app->render('index.html');
+    echo "hello world";
+});
+
+// Run app
+$app->run();
