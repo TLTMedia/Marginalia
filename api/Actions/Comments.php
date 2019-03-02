0896920a93603bef150169703e01a83153974e21
shortland <ilankleiman@gmail.com>
2019-03-01 20:50:32 -0500

    impl: Comments class for saving/deleting/getting comments on a specified works

---
+++ api/Actions/Comments.php
@@ -0,0 +1,17 @@
+<?php
+
+class Comments
+{
+    /**
+     * Save a users' comment on a specified work
+     * This method must check if the user has access to comment on specified work
+     *
+     * 1. Is the specified work 'public'?
+     *      Private works are view/edit-able by the owner only.
+     *
+     */
+    public function saveComment($pathOfWork, $comment)
+    {
+
+    }
+}
