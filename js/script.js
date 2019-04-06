db4bdbe8f086be7cc296c03649ec66deaa4669de
shortland <ilankleiman@gmail.com>
2019-04-06 02:23:04 -0400

    maint: fix edits from testing

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
   • Use the new map (commentIndexMap) that holds an 2 number array [startIndex, endIndex]
     to replace getting the comments attributes directly, reducing DOM load time
 */
-import {hello, APIHandler} from './APIHandler.js';
+
 // It should open to the literature selection window prior to initialization
 $(function() {
     getUserData().then(function(userData) {
