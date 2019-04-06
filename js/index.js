83cb05c8832e626a8e89457a04fed4fa299fc37b
shortland <ilankleiman@gmail.com>
2019-04-06 04:12:34 -0400

    test: new ini script w/modules

--- js/index.js
+++ js/index.js
@@ -4,11 +4,9 @@
 import { APIHandler, Users } from './ModuleLoader.js';
 
 (async () => {
-    const xyz = new APIHandler();
-    console.log("APIHandler is:", xyz);
-
-    const abc = await new Users({api: xyz});
-    console.log("Users is:", abc.user_list);
-    console.log("Current users is:", abc.current_user);
-    init(abc.current_user);
+    const api = new APIHandler();
+    const users = await new Users({api: api});
+    console.log("Users is:", users.user_list);
+    console.log("Current users is:", users.current_user);
+    init(users.current_user);
 })();
