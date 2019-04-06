aa29c0edcc13f55ecd4ffd8b0c23176b8d2e2517
shortland <ilankleiman@gmail.com>
2019-04-06 04:42:02 -0400

    test: new ini script w/modules

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ import { APIHandler, Users, Comments } from './ModuleLoader.js';
     const api = new APIHandler();
     const users = await new Users({api});
     //const comments = await new Comments({api});
-    console.log(users.get_user_works('ikleiman@stonybrook.edu'));
+    console.log(await users.get_user_works('ikleiman@stonybrook.edu'));
     console.log(users);
     init(users.current_user);
 })();
