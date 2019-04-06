8c1594fdc62a4239a00546de626e19c875205d15
shortland <ilankleiman@gmail.com>
2019-04-06 03:23:06 -0400

    test: new ini script w/modules

--- js/Users.js
+++ js/Users.js
@@ -12,8 +12,8 @@ export class Users {
             endpoint: 'get_users',
             method: 'get',
             data: this.grouping
-        }).then(function(userData) {
-            console.log("returned user list", user_list);
+        }).then(function(data) {
+            console.log("returned user list", data);
         });
     }
 }
