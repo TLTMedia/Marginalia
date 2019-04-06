99f64d406afcc1c470efd60b3405378c90424e60
shortland <ilankleiman@gmail.com>
2019-04-06 04:01:06 -0400

    test: new ini script w/modules

--- js/index.js
+++ js/index.js
@@ -7,6 +7,8 @@ import { APIHandler, Users } from './ModuleLoader.js';
     const xyz = new APIHandler();
     console.log("APIHandler is:", xyz);
 
+    console.log("ONLY EVER BEFORE USERS IS");
     const abc = await new Users({api: xyz});
     console.log("Users is:", abc.user_list);
+    console.log("ONLY EVER AFTER USERS IS");
 })();
