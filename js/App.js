61571b679931d905b6eda61234cbcecf3705ca81
shortland <ilankleiman@gmail.com>
2019-04-07 19:02:52 -0400

    maint: remove unnecessary object creation

--- js/App.js
+++ js/App.js
@@ -5,15 +5,14 @@ import { APIHandler, Users, Comments, RangyHelper, Dom } from './ModuleLoader.js
 
 (async () => {
     const api = new APIHandler();
-    //const dom = new Dom({api: api});
-
     const users = await new Users({api});
-    let my_works = await users.get_user_works(users.current_user['eppn']);
-    console.log(my_works);
-    console.log(users);
+    // const dom = new Dom({api: api});
+    // let my_works = await users.get_user_works(users.current_user['eppn']);
+    // console.log(my_works);
+    // console.log(users);
 
-    const comments = await new Comments({api: api, work: my_works[0], eppn: users.current_user['eppn']});
-    console.log(comments)
+    // const comments = await new Comments({api: api, work: my_works[0], eppn: users.current_user['eppn']});
+    // console.log(comments)
 
     init(users);
 })();
