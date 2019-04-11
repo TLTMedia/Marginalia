112f8e155001ecd339573680eba2acd770dceeb7
shortland <ilankleiman@gmail.com>
2019-04-10 22:08:13 -0400

    maint: fixes for posting json data

--- api/public/index.php
+++ api/public/index.php
@@ -194,9 +194,9 @@ $app->get('/set_privacy/:creator/:work/:privacy', function ($creator, $work, $pr
  */
 $app->post('/create_work', function () use ($app) {
     $json = $app->request->getBody();
-    var_dump($app->request);
+    var_dump($app->request());
     $data = json_decode($json, true);
-    var_dump($app->request);
+    var_dump($app->request());
     // if (!array_equal(array_keys($data), array('privacy', 'work'))) {
     //     echo json_encode(array(
     //         "status" => "error",
