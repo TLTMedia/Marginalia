6a5cf70733b57b7924954fe034cdb0f586fe85ce
shortland <ilankleiman@gmail.com>
2019-09-13 02:44:44 -0400

    wip: start cache implementation, rethink naming/idea of it... it's not a cache - it's a permanent directory registry...

--- api/Actions/Cache.php
+++ api/Actions/Cache.php
@@ -1,10 +1,67 @@
-<?
+<?php
 
 class Cache
 {
+    public function __construct($logger, $path, $creator, $work)
+    {
+        $this->logger = $logger;
+        $this->path = $path;
+        $this->creator = $creator;
+        $this->work = $work;
+
+        $this->cacheDir = "caches";
+    }
+
     /**
-     * TODO: Caching implemented here too
+     * Get all cache entries
      */
-    // every time a new comment is created on a work (even replies), add its path to the file
-    // public function addFullCommentPath()
+    public function getAll()
+    {
+
+    }
+
+    /**
+     * Get a cache entry
+     */
+    public function get($key)
+    {
+
+    }
+
+    /**
+     * Create a new cache entry
+     */
+    public function set($key, $value)
+    {
+
+    }
+
+    /**
+     * Remove a cache entry
+     */
+    public function clear($key)
+    {
+
+    }
+
+    /**
+     * Create the cache directory
+     * 
+     * @throws error in log
+     * @return bool
+     */
+    private function createDir()
+    {
+        if (!is_dir($this->unapprovedDirPath)) {
+            $res = mkdir($this->unapprovedDirPath, 0777, TRUE);
+            if (!$res) {
+                $this->logger->error(
+                    "Unable to create unapproved directory: ($this->unapprovedDirPath)\n
+                    Error: " . error_get_last()
+                );
+                return FALSE;
+            }
+        }
+        return TRUE;
+    }
 }
