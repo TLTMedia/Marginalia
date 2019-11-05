de3028e83a0ad476054b62103ecfb10b91b1ca1c
shortland <ilankleiman@gmail.com>
2019-11-05 14:22:08 -0500

    fix: #59

--- js/Modules/Address.js
+++ js/Modules/Address.js
@@ -20,25 +20,42 @@ export class Address {
             },
         };
 
+        // on load & change
         this.address.externalChange(() => {
-            this.process_deep_linked_url();
+            if (location.hash) {
+                this.process_deep_linked_url();
+            }
         });
+
+        // on change
+        window.onhashchange = () => {
+            if (window.innerDocClick) {
+                window.innerDocClick = false;
+            } else {
+                if (window.location.hash) {
+                    this.process_deep_linked_url();
+                } else {
+                    history.pushState("", document.title, window.location.pathname);
+                    window.location.href = window.location.href;
+                }
+            }
+        }
     }
 
     process_deep_linked_url() {
-        if (location.hash) {
-            let [, api, ...rest] = location.hash.split("#")[1].split("/");
-            if (api == "get_work") {
-                this.state.deep_link = {
-                    function: "show_work",
-                    parameters: [...rest],
-                };
-            }
-        } else {
+        let [, api, ...rest] = location.hash.split("#")[1].split("/");
+        if (api == "get_work") {
             this.state.deep_link = {
-                function: "show_home",
-                parameters: [],
+                function: "show_work",
+                parameters: [...rest],
             };
         }
     }
+
+    // process_non_deep_link() {
+    //     this.state.deep_link = {
+    //         function: "show_home",
+    //         parameters: [],
+    //     };
+    // }
 }
