e366e6cf6bd4847c8c116c0779502cd95c9c4fdb
shortland <ilankleiman@gmail.com>
2019-11-07 17:09:10 -0500

    maint: formatting

--- js/rangyHelpers.js
+++ js/rangyHelpers.js
@@ -3,12 +3,12 @@ var remSpan;
 
 
 //applies the hl to the area selected by the user
-function highlightCurrentSelection(evt,selectedType) {
+function highlightCurrentSelection(evt, selectedType) {
   var selectedRange = rangy.getSelection().getRangeAt(0);
   var lightRange = lightrange.saveSelection();
   selectedRange["nativeRange"] = lightRange;
   $("#commentBox").removeAttr("data-replyToEppn data-replyToHash");
-  $("#commentBox").attr("data-editcommentid","-1");
+  $("#commentBox").attr("data-editcommentid", "-1");
   if (selectedRange.endOffset != selectedRange.startOffset) {
     unhighlight();
     CKEDITOR.instances.textForm.setData("");
@@ -17,39 +17,39 @@ function highlightCurrentSelection(evt,selectedType) {
     CKEDITOR.instances['textForm'].setReadOnly(false);
     $(".commentTypeDropdown").removeAttr("disabled");
     console.log(range);
-    hlRange(selectedRange,range);
-    if($("."+escapeSpecialChar(remSpan)).parent().attr("class") != "commented-selection"){
+    hlRange(selectedRange, range);
+    if ($("." + escapeSpecialChar(remSpan)).parent().attr("class") != "commented-selection") {
       $("#replies").parent().hide();
       $(".loader").hide();
-      displayCommentBox(evt,selectedType);
+      displayCommentBox(evt, selectedType);
     }
   }
 }
 
-function unhighlight(){
-  remSpan ="hl_" + currentUser.eppn;
+function unhighlight() {
+  remSpan = "hl_" + currentUser.eppn;
   console.log(remSpan);
-  var text = $("."+escapeSpecialChar(remSpan)).text();
-  $("."+escapeSpecialChar(remSpan)).contents().unwrap();
+  var text = $("." + escapeSpecialChar(remSpan)).text();
+  $("." + escapeSpecialChar(remSpan)).contents().unwrap();
   return text;
 }
 
-function hlRange(selectedRange,range) {
+function hlRange(selectedRange, range) {
   remSpan = ("hl_" + currentUser.eppn);
   let applierCount = rangy.createClassApplier(remSpan, {
     useExistingElements: false,
     elementAttributes: {
-        "startIndex": range.start,
-        "endIndex": range.end,
+      "startIndex": range.start,
+      "endIndex": range.end,
     }
   });
-  console.log("lit",$("#textSpace"));
+  console.log("lit", $("#textSpace"));
   applierCount.applyToRange(selectedRange);
   return remSpan;
 }
 
-function escapeSpecialChar(id){
-  if(id == null){
+function escapeSpecialChar(id) {
+  if (id == null) {
     return null;
   }
   return id.replace(/([\s!"#$%&'()\*+,\.\/:;<=>?@\[\]^`{|}~])/g, "\\$1");
